83e3fb0f2580b0f48f944e6c79b93146f42789e7
Steven Turturo <steven@turturo.dev>
2026-07-02 20:44:35 -0400

    fix: undo some schema changes

--- examples/dynamicquery/sqlite/schema.sql
+++ examples/dynamicquery/sqlite/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE records (
-    id         INTEGER PRIMARY KEY,
-    tenant_id  BIGINT NOT NULL,
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    tenant_id  INTEGER NOT NULL,
     name       TEXT NOT NULL,
-    age        INT NOT NULL,
+    age        INTEGER NOT NULL,
     status     TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
